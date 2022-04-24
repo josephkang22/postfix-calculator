1160f2a0270c533009c10dfe834df4863207262d
Joseph Kang <frothekangaroo@gmail.com>
2022-04-24 11:32:35 +0900

    fixed indentation

--- main.c
+++ main.c
@@ -7,31 +7,30 @@ int stack[100];
 int stack_index=0;
 
 void push(int x) {
-    stack[stack_index] = x;
-    stack_index = stack_index+1;
+		stack[stack_index] = x;
+		stack_index = stack_index+1;
 }
 
 int pop () {
-    stack_index = stack_index-1;
-    return stack[stack_index];
+		stack_index = stack_index-1;
+		return stack[stack_index];
 }
 
 int main() {
 	gets(input);
 	len = strlen(input);
 	for (int i=0; i<len; i++) {
-  	if (input[i] == '+') {
-			int x = pop();		
-		 	int y = pop();
-   		int sum = x + y;
-      push(sum);     
-    } else if (input[i] == '-') {
-           
-			int z =	pop();
-			int sum =	pop(); 
+		if (input[i] == '+') {
+			int x = pop();
+			int y = pop();
+			int sum = x + y;
+			push(sum);
+		} else if (input[i] == '-') {
+			int z = pop();
+			int sum = pop(); 
 			int difference = sum - z;
 			push(difference);
-    } else if (input[i] == '*') {
+		} else if (input[i] == '*') {
 			
 			int x = pop();
 			int y = pop();
@@ -46,12 +45,12 @@ int main() {
 			push(quotient);
 			
 			
-    } else {
-      int num = input[i] - '0'; // Value of the number at input[i]
-      push(num);
-    } 
-  }
-  printf("Final output, %d\n", pop());
+		} else {
+			int num = input[i] - '0'; // Value of the number at input[i]
+			push(num);
+		} 
+	}
+	printf("Final output, %d\n", pop());
 }
  
 
